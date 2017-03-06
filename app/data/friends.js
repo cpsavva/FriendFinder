8f24b49121d3852ab50018d8c8e14ba82031f6cc
cpsavva <csoriasavva@gmail.com>
2017-03-06 15:10:49 -0500

    priliminary work on the comparison logic of the assignment

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,20 @@
 
 
-
-
-
-
-var survey = [];
+var survey = [{
+	'friendName': 'Larry',
+	'friendPhotoURL':'http://www.threestooges.com/wp-content/uploads/2012/12/LarryFine.jpg',
+	'score':[1,2,3,4,5,4,3,2,1,5,]
+},
+{
+'friendName': 'Moe',
+	'friendPhotoURL':'http://www.threestooges.com/wp-content/uploads/2012/12/MoeHoward.jpg',
+	'score':[2,3,4,5,2,4,1,3,3,1]	
+},
+{
+'friendName': 'Curly',
+	'friendPhotoURL':'http://www.threestooges.com/wp-content/uploads/2012/12/CurlyHoward.jpg',
+	'score':[5,5,4,4,3,3,2,2,1,1]
+},
+];
 
 module.exports = survey;
